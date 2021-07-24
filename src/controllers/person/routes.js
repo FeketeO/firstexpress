1c0dcd73e4198053c6aabe576d571d5085cebd24
FeketeO <69634719+FeketeO@users.noreply.github.com>
2021-07-24 23:06:09 +0200

    CRUD, testing

--- src/controllers/person/routes.js
+++ src/controllers/person/routes.js
@@ -6,6 +6,50 @@ const controller = express.Router();
 controller.get('/', (req, res) => {
     res.json(data)
 });
+// a fő url itt a / person
+
+// get only one person
+controller.get('/:id', (req, res) => {
+    const person = data.find(person => person.id === Number(req.params.id))
+    res.json(person)
+});
 
 
+
+
+// create a new person
+
+controller.post('/', (req, res) => {
+    const newPerson = req.body;
+    newPerson.id = data[data.length -1].id + 1;
+    data.push(newPerson);
+
+    res.status(201);
+    res.json(newPerson)
+});
+
+// update
+controller.put('/:id', (req, res) => {
+    const id = req.params.id;
+    const index = data.findIndex(person => person.id === Number(id))
+    // azért kell betenni number konstruktorba, mert a mongo stringként tárolja az IDkat
+    const { first_name, last_name, email} = req.body
+    // másik lehetőség: first_name = req.body.firstName, stb. Ehelyett dekonstruálással
+    data[index] = {
+        id,
+        first_name,
+        last_name,
+        email
+    }
+
+    res.status(200);
+    res.json(data[index]);
+});
+
+// delete one person
+controller.delete('/:id', (req, res) => {
+    const index = data.findIndex(person => person.id === Number(req.params.id))
+    data.splice(index, 1);
+    res.json({})
+});
 module.exports = controller;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const app = express();
 const port = 3000;
+const bodyParser = require('body-parser')
 
 // app.get('/', (req, res) => {
 //     res.send('Hello World');
 
 // });
-
+app.use(bodyParser.json());
 app.use('/person', require('./controllers/person/routes'));
 
 app.listen(port, () => {

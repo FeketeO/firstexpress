755c71570a0000200262e9638673f4bc2d92e740
FeketeO <69634719+FeketeO@users.noreply.github.com>
2021-07-27 23:11:23 +0200

    mongoose.Schema

---
+++ src/models/person.model.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const PersonSchema = mongoose.Schema({
+    firstName: String,
+    lastName: String,
+    email: String
+},
+{
+    timeStamps: true
+});
+
+module.exports = mongoose.model('Person', PersonSchema )
